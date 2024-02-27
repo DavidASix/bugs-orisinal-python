e3ed28ded2f8fc16c36e0dc2c5de8c569aa95526
David Anderson <davidandersonsix@gmail.com>
2024-02-26 19:07:52 -0500

    Added padding for bugs to spawn into, created random initial seed so they spawn at all sides of screen

--- src/bug.py
+++ src/bug.py
@@ -1,23 +1,34 @@
 import pygame
 import random
 import math
+import constants as c
 
 class Bug:
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.x = random.randint(0, self.screen_width)
-        self.y = random.randint(0, self.screen_height)
-        self.speed = 1.8 # pixels per frame to move
+        state_seed = random.randint(0, 3);
+        initial_states = [
+            (-20, random.randint(0, self.screen_height)),
+            (self.screen_width + 20, random.randint(0, self.screen_height)),
+            (random.randint(0, self.screen_width), -20),
+            (random.randint(0, self.screen_width), self.screen_height + 20),
+        ]
+        self.x, self.y = initial_states[state_seed]
+        self.speed = 1 # pixels per frame to move
         self.angle = random.randint(0, 360)
+        self.color = [c.RED, c.BLUE, c.GREEN][random.randint(0, 2)]
         self.size = 20
 
     def move(self):
         self.x += self.speed * math.cos(math.radians(self.angle))
         self.y += self.speed * math.sin(math.radians(self.angle))
-
-        self.x = (self.x + self.screen_width) % self.screen_width
-        self.y = (self.y + self.screen_height) % self.screen_height
+        screen_padding = self.size * 2
+        width = self.screen_width + screen_padding
+        height = self.screen_height + screen_padding
+        min_location = -screen_padding/2
+        self.x = (self.x - min_location + width) % width - screen_padding / 2
+        self.y = (self.y - min_location + height) % height - screen_padding / 2
 
         self.angle += random.randint(-10, 10)
         if self.angle < 0:
@@ -26,5 +37,5 @@ class Bug:
             self.angle -= 360
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y),  10)
+        pygame.draw.circle(screen, self.color, (self.x, self.y),  10)
         #pygame.draw.line(screen, (0, 0, 0), (self.x, self.y), (self.x + self.size * math.cos(math.radians(self.angle)), self.y + self.size * math.sin(math.radians(self.angle))), 2)
--- src/game.py
+++ src/game.py
@@ -18,7 +18,7 @@ class Game:
         width, height = self.screen.get_size()
         player = Player()
         bubble = None
-        bug = Bug(width, height)
+        bugs = []
         while self.running:
             # Event handling
             for event in pygame.event.get():
@@ -33,17 +33,24 @@ class Game:
                         bubble = Bubble()
                 elif event.type == pygame.MOUSEBUTTONUP:
                         bubble.flash_counter = 1
-            
-            # PLAYER AND BUBBLE LOGIC
+            while len(bugs) < 20:
+                bugs.append(Bug(width, height))
+
+            self.screen.fill(c.WHITE)
+
+            # BUG LOGIC
+            for bug in bugs:
+                bug.move()
+                bug.draw(self.screen)
+                
+            # Player Logic
             # Get the mouse position and update player facing direction
             current_mouse_pos = pygame.mouse.get_pos()
             player.update_direction(self.last_mouse_pos, current_mouse_pos)
             self.last_mouse_pos = pygame.mouse.get_pos()
-
             player.move_toward_mouse(height, current_mouse_pos)
 
-            # Draw the game
-            self.screen.fill(c.WHITE)
+            # Bubble Logic
             if bubble is not None:
                 bubble.show_flash_on_release()
                 bubble.grow_circle_on_click()
@@ -54,9 +61,6 @@ class Game:
                     
             player.draw(self.screen)
 
-            # BUG LOGIC
-            bug.move()
-            bug.draw(self.screen)
 
             # Update the display
             pygame.display.flip()
