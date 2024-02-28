757f6fd290919733b2c542b881104e165590abc2
David Anderson <davidandersonsix@gmail.com>
2024-02-27 21:16:23 -0500

    Added textures to bugs

--- src/bug.py
+++ src/bug.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import math
 import constants as c
+import os
 
 class Bug:
     def __init__(self, screen_width, screen_height):
@@ -15,10 +16,13 @@ class Bug:
             (random.randint(0, self.screen_width), self.screen_height + 20),
         ]
         self.x, self.y = initial_states[state_seed]
-        self.speed = 1 # pixels per frame to move
+        self.speed = 1.25 # pixels per frame to move
         self.angle = random.randint(0, 360)
-        self.color = [c.RED, c.BLUE, c.GREEN][random.randint(0, 2)]
+        self.color = ['red', 'blue', 'green', 'yellow', 'orange'][random.randint(0, 4)]
         self.size = 20
+        current_dir = os.path.dirname(__file__)
+        image_path = os.path.join(current_dir, f'assets/sprites/bugs/{self.color}.png')
+        self.image = pygame.transform.scale(pygame.image.load(image_path), (self.size, self.size))
 
     def move(self):
         self.x += self.speed * math.cos(math.radians(self.angle))
@@ -30,12 +34,12 @@ class Bug:
         self.x = (self.x - min_location + width) % width - screen_padding / 2
         self.y = (self.y - min_location + height) % height - screen_padding / 2
 
-        self.angle += random.randint(-10, 10)
+        self.angle += random.randint(-2, 2)
         if self.angle < 0:
             self.angle += 360
         if self.angle > 360:
             self.angle -= 360
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y),  10)
-        #pygame.draw.line(screen, (0, 0, 0), (self.x, self.y), (self.x + self.size * math.cos(math.radians(self.angle)), self.y + self.size * math.sin(math.radians(self.angle))), 2)
+        rotated_image = pygame.transform.rotate(self.image, 90-self.angle)
+        screen.blit(rotated_image, (self.x - self.size / 2, self.y - self.size / 2))
