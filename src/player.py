a2857ab65cfcb3c4cdf39cffd1defad8c166eefa
David Anderson <davidandersonsix@gmail.com>
2024-03-04 08:21:49 -0500

    Added jumping animation

--- src/player.py
+++ src/player.py
@@ -7,18 +7,18 @@ import constants as c
 class Player:
     def __init__(self, screen):
         self.screen = screen
-        self.size = 20
+        self.size = 25
         self.x = (c.WIDTH + self.size) / 2
         self.y = (c.HEIGHT + self.size) / 2
-        self.color = c.GREEN
 
         self.direction = "S"
 
         self.moving = False
         self.jumping = False
         self.invincible = 0
+        self.jump_timer = 1
+        self.jump_index = 0
         self.ouch_animation = None
-        self.jump_animation = None
         self.moving_animation_index = 0
         self.play_field = pygame.Surface(screen.get_size())
 
@@ -32,6 +32,11 @@ class Player:
                 image_path = f'{animation_folder}/{i}.png'
                 image = pygame.image.load(image_path)
                 self.walking_animations[direction].append(image)
+        
+        self.jumping_frames = []
+        for i in range(32):
+            image_path = f'./assets/sprites/player/jump/{i+1}.png'
+            self.jumping_frames.append(pygame.image.load(image_path))
 
 
     def update_direction(self):
@@ -73,7 +78,7 @@ class Player:
 
         target_x, target_y = current_mouse_pos
         # Player should take 2.5 seconds to go from top to bottom of frame
-        frame_speed = frame_height / 3.75 / c.FPS
+        frame_speed = frame_height / 3.5 / c.FPS
         distance = math.hypot(target_x - self.x, target_y - self.y)
         # distance is compared against frame height to account for any variance
         # in movement when the frame is larger.
@@ -90,27 +95,50 @@ class Player:
             self.x -= (self.x - circle_center[0]) * (player_distance - circle_radius) / player_distance
             self.y -= (self.y - circle_center[1]) * (player_distance - circle_radius) / player_distance
     
+    def calculate_jump(self):
+        if self.jumping:
+            # Calculate length of animation and current frame to display
+            jump_length = 0.75
+            jump_frame_length = c.FPS * jump_length
+            jump_frame_interval = jump_frame_length / (len(self.jumping_frames) -1)
+            self.jump_index = math.floor(self.jump_timer / jump_frame_interval)
+
+            # Increment timer
+            if (self.jump_timer < jump_frame_length):
+                self.jump_timer += 1
+        else:
+            self.jump_timer = 0
+    
     def animation(self):
         directions = ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW']
-        if self.direction in directions:
-            diameter = self.size * 2
+        # Handle jumping animation
+        if self.jumping:
+            height = self.size * 4.5
+            width = self.size * 4.5 * 0.75
+            # Setup image
+            image = self.jumping_frames[self.jump_index]
+            image = pygame.transform.smoothscale(image, (width, height))
+            self.screen.blit(image, (int(self.x - (width/2)), int(self.y - (height/3*2))))
+            # Handle walking animation
+        elif self.direction in directions:
+            height = self.size * 2.25
+            width = self.size * 2.25 * 0.75
             image = self.walking_animations[self.direction][self.moving_animation_index]
-            image = pygame.transform.smoothscale(image, (diameter, diameter))
-            self.screen.blit(image, (int(self.x - self.size), int(self.y - self.size)))
+            image = pygame.transform.smoothscale(image, (width, height))
+            self.screen.blit(image, (int(self.x - (width / 2)), int(self.y - (height / 2))))
             # Update animation frame
             if self.moving:
                 self.moving_animation_index += 1
                 self.moving_animation_index %= len(self.walking_animations[self.direction])
+        # Handle the case where self.direction is not a valid direction
         else:
-            # Handle the case where self.direction is not a valid direction
-            self.color = c.RED if self.jumping else c.GREEN
-            pygame.draw.circle(self.screen, self.color, (int(self.x), int(self.y)), self.size)
+            pygame.draw.circle(self.screen, c.GREEN, (int(self.x), int(self.y)), self.size)
 
     def draw(self):
         flashing_seconds = 3
         flashing_frames = flashing_seconds * c.FPS
         flashing_interval = 3
-
+        self.calculate_jump()
         # If the player is invincible, flash every 3 frames for 3 seconds
         if self.invincible and self.invincible < flashing_frames:
             if self.invincible % flashing_interval == 0:  
